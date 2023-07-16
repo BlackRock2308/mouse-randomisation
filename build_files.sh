6edc353e5f719590c0ad0d329c3ca182484dce68
MBAYE SENE <smbaye@ept.sn>
2023-07-16 21:06:44 +0000

    updating my database information

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,10 @@
 
 echo "BUILD START"
 pip install -r requirements.txt
+
+echo "Make migration"
+python3.9 manage.py makemigrations --noinput 
+python3.9 manage.py migrate --noinput 
+
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
